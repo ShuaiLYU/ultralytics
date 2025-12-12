5d55f9e4504ca84449919c70f4984fde336747df
ShuaiLYU <shuai.lyu@foxmail.com>
2025-12-12 03:12:44 -0600

    Delete nc=self.data["nc"] in Format.__init__

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -239,7 +239,6 @@ class YOLODataset(BaseDataset):
                 mask_overlap=hyp.overlap_mask,
                 bgr=hyp.bgr if self.augment else 0.0,  # only affect training.
                 semseg_loss=hyp.semseg_loss if "train" in self.prefix else False,
-                nc=self.data["nc"]
             )
         )
         return transforms
